907fadb166f846ba4b5ba8d55c22e77e8663ad9f
Restyled.io <commits@restyled.io>
2024-01-03 14:29:16 +0000

    Restyled by clang-format

--- include/class_loader/meta_object.hpp
+++ include/class_loader/meta_object.hpp
@@ -41,129 +41,126 @@
 
 #include "class_loader/visibility_control.hpp"
 
-namespace class_loader
-{
+namespace class_loader {
 
-class ClassLoader;  // Forward declaration
+class ClassLoader; // Forward declaration
 
-namespace impl
-{
+namespace impl {
 
 class AbstractMetaObjectBaseImpl;
 
 /**
  * @class AbstractMetaObjectBase
- * @brief A base class for MetaObjects that excludes a polymorphic type parameter. Subclasses are class templates though.
+ * @brief A base class for MetaObjects that excludes a polymorphic type
+ * parameter. Subclasses are class templates though.
  */
-class CLASS_LOADER_PUBLIC AbstractMetaObjectBase
-{
+class CLASS_LOADER_PUBLIC AbstractMetaObjectBase {
 public:
-    /**
-     * @brief Constructor for the class
-     */
-    AbstractMetaObjectBase(
-        const std::string & class_name,
-        const std::string & base_class_name,
-        const std::string & typeid_base_class_name = "UNSET");
-    /**
-     * @brief Destructor for the class. THIS MUST NOT BE VIRTUAL AND OVERRIDDEN BY
-     * TEMPLATE SUBCLASSES, OTHERWISE THEY WILL PULL IN A REDUNDANT METAOBJECT
-     * DESTRUCTOR OUTSIDE OF libclass_loader WITHIN THE PLUGIN LIBRARY! T
-     */
-    ~AbstractMetaObjectBase();
+  /**
+   * @brief Constructor for the class
+   */
+  AbstractMetaObjectBase(const std::string &class_name,
+                         const std::string &base_class_name,
+                         const std::string &typeid_base_class_name = "UNSET");
+  /**
+   * @brief Destructor for the class. THIS MUST NOT BE VIRTUAL AND OVERRIDDEN BY
+   * TEMPLATE SUBCLASSES, OTHERWISE THEY WILL PULL IN A REDUNDANT METAOBJECT
+   * DESTRUCTOR OUTSIDE OF libclass_loader WITHIN THE PLUGIN LIBRARY! T
+   */
+  ~AbstractMetaObjectBase();
 
-    /**
-     * @brief Gets the literal name of the class.
-     * @return The literal name of the class as a C-string.
-     */
-    const std::string & className() const;
+  /**
+   * @brief Gets the literal name of the class.
+   * @return The literal name of the class as a C-string.
+   */
+  const std::string &className() const;
 
-    /**
-     * @brief gets the base class for the class this factory represents
-     */
-    const std::string & baseClassName() const;
-    /**
-     * @brief Gets the name of the class as typeid(BASE_CLASS).name() would return it
-     */
-    const std::string & typeidBaseClassName() const;
+  /**
+   * @brief gets the base class for the class this factory represents
+   */
+  const std::string &baseClassName() const;
+  /**
+   * @brief Gets the name of the class as typeid(BASE_CLASS).name() would return
+   * it
+   */
+  const std::string &typeidBaseClassName() const;
 
-    /**
-     * @brief Gets the path to the library associated with this factory
-     * @return Library path as a std::string
-     */
-    const std::string & getAssociatedLibraryPath() const;
+  /**
+   * @brief Gets the path to the library associated with this factory
+   * @return Library path as a std::string
+   */
+  const std::string &getAssociatedLibraryPath() const;
 
-    /**
-     * @brief Sets the path to the library associated with this factory
-     */
-    void setAssociatedLibraryPath(const std::string & library_path);
+  /**
+   * @brief Sets the path to the library associated with this factory
+   */
+  void setAssociatedLibraryPath(const std::string &library_path);
 
-    /**
-     * @brief Associates a ClassLoader owner with this factory,
-     * @param loader Handle to the owning ClassLoader.
-     */
-    void addOwningClassLoader(ClassLoader * loader);
+  /**
+   * @brief Associates a ClassLoader owner with this factory,
+   * @param loader Handle to the owning ClassLoader.
+   */
+  void addOwningClassLoader(ClassLoader *loader);
 
-    /**
-     * @brief Removes a ClassLoader that is an owner of this factory
-     * @param loader Handle to the owning ClassLoader.
-     */
-    void removeOwningClassLoader(const ClassLoader * loader);
+  /**
+   * @brief Removes a ClassLoader that is an owner of this factory
+   * @param loader Handle to the owning ClassLoader.
+   */
+  void removeOwningClassLoader(const ClassLoader *loader);
 
-    /**
-     * @brief Indicates if the factory is within the usable scope of a ClassLoader
-     * @param loader Handle to the owning ClassLoader.
-     */
-    bool isOwnedBy(const ClassLoader * loader) const;
+  /**
+   * @brief Indicates if the factory is within the usable scope of a ClassLoader
+   * @param loader Handle to the owning ClassLoader.
+   */
+  bool isOwnedBy(const ClassLoader *loader) const;
 
-    /**
-     * @brief Indicates if the factory is within the usable scope of any ClassLoader
-     */
-    bool isOwnedByAnybody() const;
+  /**
+   * @brief Indicates if the factory is within the usable scope of any
+   * ClassLoader
+   */
+  bool isOwnedByAnybody() const;
 
-    size_t getAssociatedClassLoadersCount() const;
+  size_t getAssociatedClassLoadersCount() const;
 
-    ClassLoader * getAssociatedClassLoader(size_t index) const;
+  ClassLoader *getAssociatedClassLoader(size_t index) const;
 
 protected:
-    /**
-     * This is needed to make base class polymorphic (i.e. have a vtable)
-     */
-    virtual void dummyMethod() {}
+  /**
+   * This is needed to make base class polymorphic (i.e. have a vtable)
+   */
+  virtual void dummyMethod() {}
 
-    AbstractMetaObjectBaseImpl * impl_;
+  AbstractMetaObjectBaseImpl *impl_;
 };
 
 /**
  * @class AbstractMetaObject
- * @brief Abstract base class for factories where polymorphic type variable indicates base class for plugin interface.
+ * @brief Abstract base class for factories where polymorphic type variable
+ * indicates base class for plugin interface.
  * @parm B The base class interface for the plugin
  */
-template<class B>
-class AbstractMetaObject : public AbstractMetaObjectBase
-{
+template <class B> class AbstractMetaObject : public AbstractMetaObjectBase {
 public:
-    /**
-     * @brief A constructor for this class
-     * @param name The literal name of the class.
-     */
-    AbstractMetaObject(const std::string & class_name, const std::string & base_class_name)
-        : AbstractMetaObjectBase(class_name, base_class_name, typeid(B).name())
-    {
-    }
+  /**
+   * @brief A constructor for this class
+   * @param name The literal name of the class.
+   */
+  AbstractMetaObject(const std::string &class_name,
+                     const std::string &base_class_name)
+      : AbstractMetaObjectBase(class_name, base_class_name, typeid(B).name()) {}
 
-    /**
-     * @brief Defines the factory interface that the MetaObject must implement.
-     * @return A pointer of parametric type B to a newly created object.
-     */
-    virtual B * create() const = 0;
-    /// Create a new instance of a class.
-    /// Cannot be used for singletons.
+  /**
+   * @brief Defines the factory interface that the MetaObject must implement.
+   * @return A pointer of parametric type B to a newly created object.
+   */
+  virtual B *create() const = 0;
+  /// Create a new instance of a class.
+  /// Cannot be used for singletons.
 
 private:
-    AbstractMetaObject();
-    AbstractMetaObject(const AbstractMetaObject &);
-    AbstractMetaObject & operator=(const AbstractMetaObject &);
+  AbstractMetaObject();
+  AbstractMetaObject(const AbstractMetaObject &);
+  AbstractMetaObject &operator=(const AbstractMetaObject &);
 };
 
 /**
@@ -172,29 +169,24 @@ private:
  * @parm C The derived class (the actual plugin)
  * @parm B The base class interface for the plugin
  */
-template<class C, class B>
-class MetaObject : public AbstractMetaObject<B>
-{
+template <class C, class B> class MetaObject : public AbstractMetaObject<B> {
 public:
-    /**
-     * @brief Constructor for the class
-     */
-    MetaObject(const std::string & class_name, const std::string & base_class_name)
-        : AbstractMetaObject<B>(class_name, base_class_name)
-    {
-    }
+  /**
+   * @brief Constructor for the class
+   */
+  MetaObject(const std::string &class_name, const std::string &base_class_name)
+      : AbstractMetaObject<B>(class_name, base_class_name) {}
 
-    /**
-     * @brief The factory interface to generate an object. The object has type C in reality, though a pointer of the base class type is returned.
-     * @return A pointer to a newly created plugin with the base class type (type parameter B)
-     */
-    B * create() const
-    {
-        return new C;
-    }
+  /**
+   * @brief The factory interface to generate an object. The object has type C
+   * in reality, though a pointer of the base class type is returned.
+   * @return A pointer to a newly created plugin with the base class type (type
+   * parameter B)
+   */
+  B *create() const { return new C; }
 };
 
-}  // namespace impl
-}  // namespace class_loader
+} // namespace impl
+} // namespace class_loader
 
-#endif  // CLASS_LOADER__META_OBJECT_HPP_
+#endif // CLASS_LOADER__META_OBJECT_HPP_
